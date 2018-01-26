f2369c84c37885e76ba7c205bd4924c1f6d69724
Kurt Eigenstiller <kurt.eigenstiller@schule.at>
2018-01-26 05:08:53 +0100

    1d EditText

--- app/src/main/java/com/einkarat/maxreps/MainActivity.java
+++ app/src/main/java/com/einkarat/maxreps/MainActivity.java
@@ -3,8 +3,8 @@ package com.einkarat.maxreps;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.SeekBar;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SeekBar seekBar_gewicht = (SeekBar) findViewById(R.id.seekBar_gewicht);
+        SeekBar seekBar_gewicht = findViewById(R.id.seekBar_gewicht);
         // you should define max in xml, but if you need to do this by code, you must set max as 0 and then your desired value. this is because a bug in SeekBar (issue 12945) (don't really checked if it was corrected)
         seekBar_gewicht.setMax(0);
         seekBar_gewicht.setMax(50);
         seekBar_gewicht.setProgress(10);
 
-        final TextView zahl_gewicht = (TextView) findViewById(R.id.gewicht_zahl);
+        final EditText zahl_gewicht = (EditText) findViewById(R.id.gewicht_zahl);
         int pro=seekBar_gewicht.getProgress();
         zahl_gewicht.setText(String.valueOf(pro));
 
@@ -43,13 +43,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        SeekBar seekBar_wh = (SeekBar) findViewById(R.id.seekBar_wh);
+        SeekBar seekBar_wh = findViewById(R.id.seekBar_wh);
         // you should define max in xml, but if you need to do this by code, you must set max as 0 and then your desired value. this is because a bug in SeekBar (issue 12945) (don't really checked if it was corrected)
         seekBar_wh.setMax(0);
         seekBar_wh.setMax(50);
         seekBar_wh.setProgress(10);
 
-        final TextView zahl_wh = (TextView) findViewById(R.id.wh_zahl);
+        final EditText zahl_wh = findViewById(R.id.wh_zahl);
         int pro_wh=seekBar_gewicht.getProgress();
         zahl_wh.setText(String.valueOf(pro_wh));
 
